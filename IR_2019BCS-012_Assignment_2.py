0572babd5452fdf752edc634c86fa1136ede2a7a
Bharat Chandwani <56021613+bhch1507@users.noreply.github.com>
2021-03-03 13:24:12 +0530

    Update IR_2019BCS-012_Assignment_2.py

--- IR_2019BCS-012_Assignment_2.py
+++ IR_2019BCS-012_Assignment_2.py
@@ -4,23 +4,58 @@
 
 import numpy as np
 import math as m
+
+
+Bp = list(map(float, input(
+    "\nEnter the coordinates(final coordinates) : ").strip().split()))
+check = 'y'
+RotationMatrix = np.identity(3)
+Bp = np.asarray(np.reshape(Bp, [3, 1]), dtype=float)
+
+
 def Rx(theta):
-  return np.matrix([[ 1, 0           , 0           ],
-                   [ 0, m.cos(theta),-m.sin(theta)],
-                   [ 0, m.sin(theta), m.cos(theta)]])
-  
+    return np.matrix([[1, 0, 0],
+                      [0, m.cos(theta), -m.sin(theta)],
+                      [0, m.sin(theta), m.cos(theta)]])
+
+
 def Ry(theta):
-  return np.matrix([[ m.cos(theta), 0, m.sin(theta)],
-                   [ 0           , 1, 0           ],
-                   [-m.sin(theta), 0, m.cos(theta)]])
-  
+    return np.matrix([[m.cos(theta), 0, m.sin(theta)],
+                      [0, 1, 0],
+                      [-m.sin(theta), 0, m.cos(theta)]])
+
+
 def Rz(theta):
-  return np.matrix([[ m.cos(theta), -m.sin(theta), 0 ],
-                   [ m.sin(theta), m.cos(theta) , 0 ],
-                   [ 0           , 0            , 1 ]])
+    return np.matrix([[m.cos(theta), -m.sin(theta), 0],
+                      [m.sin(theta), m.cos(theta), 0],
+                      [0, 0, 1]])
 
-Bp = np.matrix([2,3,0])
 
-Ap = Rx(30)*Ry(30)*Ry(30)*np.transpose(Bp);
+while check == 'y':
+    angle, axis = list(
+        input("\nEnter the angle and axis of rotation: ").split())
+    angle = m.radians(float(angle))
 
-print(np.round(Ap, 3))
+    if(axis == "x" or axis == "X"):
+        matrix = Rx(angle)
+        RotationMatrix = RotationMatrix*matrix
+
+    elif(axis == "y" or axis == "Y"):
+        matrix = Ry(angle)
+        RotationMatrix = RotationMatrix*matrix
+
+    elif(axis == "z" or axis == "Z"):
+        matrix = Rz(angle)
+        RotationMatrix = RotationMatrix*matrix
+
+    else:
+        print("Invalid axis: ")
+
+    check = input("Enter y to continue to add more rotations: ")
+
+Ap = RotationMatrix*Bp
+Ap = np.round(Ap, 3)
+
+print("Final coodinatess after Rotation are :")
+for i in Ap:
+    print(i, end=', ')
