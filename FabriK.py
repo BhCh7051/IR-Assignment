a9322948c7f73010d807daa58b64fd46f0b90486
Bharat Chandwani <56021613+bhch1507@users.noreply.github.com>
2021-04-03 12:23:51 +0530

    refactored

--- FabriK.py
+++ FabriK.py
@@ -3,9 +3,11 @@
 
 
 import math
-from matplotlib.pyplot import plot, show, style, text
-import matplotlib.pyplot as plt
+
 import numpy as np
+from matplotlib.pyplot import plot, show, style
+
+
 # Input: The joint positions pi for i = 1,...,n, the
 # target position target and the distances between each
 # joint
@@ -34,7 +36,7 @@ class Point(object):
     def distance(self, other):
         dx = self.X - other.X
         dy = self.Y - other.Y
-        return math.sqrt(dx**2 + dy**2)
+        return math.sqrt(dx ** 2 + dy ** 2)
 
 
 def fabrik():
@@ -45,15 +47,15 @@ def fabrik():
     sum = 0
     for i in range(n):
         x, y = list(
-            map(float, input("Enter {} coordinate: ".format(i+1)).strip().split(),))[:3]
+            map(float, input("Enter {} coordinate: ".format(i + 1)).strip().split(), ))[:3]
         p.append(Point(x, y))
 
-    for i in range(n-1):
-        plot([p[i-1].getX(), p[i].getX()],  [p[i-1].getY(), p[i].getY()],
+    for i in range(1,n):
+        plot([p[i - 1].getX(), p[i].getX()], [p[i - 1].getY(), p[i].getY()],
              color='red', marker='X', linestyle=':', linewidth=1)
 
     x, y = list(
-        map(float, input("Enter Goal coordinate: ").strip().split(),))[:3]
+        map(float, input("Enter Goal coordinate: ").strip().split(), ))[:3]
     target = (Point(x, y))
     plot(target.getX(), target.getY(), 'X', label='Goal')
     final = target.getXY()
@@ -61,17 +63,17 @@ def fabrik():
     iteration = 0
     d = []
     for i in range(1, n):
-        d.append(p[i].distance(p[i-1]))
-        sum += p[i].distance(p[i-1])
-    if(sum < target.distance(p[0])):
+        d.append(p[i].distance(p[i - 1]))
+        sum += p[i].distance(p[i - 1])
+    if sum < target.distance(p[0]):
         print(" The target is unreachable")
 
-        for i in range(0, n-1):
+        for i in range(0, n - 1):
             # Find the distance ri between the target target and the joint
             r = target.distance(p[i])
-            lambd = d[i-1]/r
-            arr = ((1-lambd)*(p[i].getXY()) + lambd*(target.getXY())).tolist()
-            p[i+1] = Point(arr[0], arr[1])
+            lambd = d[i - 1] / r
+            arr = ((1 - lambd) * (p[i].getXY()) + lambd * (target.getXY())).tolist()
+            p[i + 1] = Point(arr[0], arr[1])
     else:
         dist = p[-1].distance(target)
         while dist > tol:
@@ -79,30 +81,30 @@ def fabrik():
             # STAGE 1: FORWARD REACHING
             # Set the end effector pn as target target
             p[-1] = Point(final[0], final[1])
-            for i in range(n-2, 0, -1):
-                r = p[i].distance(p[i+1])
-                lambd = d[i]/r
-                arr = ((1-lambd)*p[i+1].getXY()+lambd*p[i].getXY()).tolist()
+            for i in range(n - 2, 0, -1):
+                r = p[i].distance(p[i + 1])
+                lambd = d[i] / r
+                arr = ((1 - lambd) * p[i + 1].getXY() + lambd * p[i].getXY()).tolist()
                 p[i] = Point(arr[0], arr[1])
 
             # STAGE 2: BACKWARD REACHING
             # Set the root p1 its initial position
             initial = p[0].getXY()
-            for i in range(0, n-1):
+            for i in range(0, n - 1):
                 p[0] = Point(initial[0], initial[1])
-                r = p[i+1].distance(p[i])
-                lambd = d[i]/r
-                arr = ((1-lambd)*p[i].getXY()+lambd*p[i+1].getXY()).tolist()
-                p[i+1] = Point(arr[0], arr[1])
+                r = p[i + 1].distance(p[i])
+                lambd = d[i] / r
+                arr = ((1 - lambd) * p[i].getXY() + lambd * p[i + 1].getXY()).tolist()
+                p[i + 1] = Point(arr[0], arr[1])
 
             dist = p[-1].distance(target)
             for i in range(1, n):
-                plot([p[i-1].getX(), p[i].getX()],  [p[i-1].getY(), p[i].getY()],
-                     marker='o', linestyle='dashed', linewidth=1, label='Line{}'.format(i+1))
+                plot([p[i - 1].getX(), p[i].getX()], [p[i - 1].getY(), p[i].getY()],
+                     marker='o', linestyle='dashed', linewidth=1, label='Line{}'.format(i + 1))
     show(block=True)
     print("------Results------")
     print("Iterations: ", iteration)
-    print("Link position:",)
+    print("Link position:", )
     for i in range(n):
         print(p[i])
     print("Goal Position:", target)
--- README.md
+++ README.md
@@ -1,7 +1,4 @@
-# IR Assignment 2
 # 2019BCS-012  Bharat Chandwani
 # BCS1923
 ### IR Assignment
-
-Here Bp = (2, 3, 0) and frame B is obtained by rotating along X axis by 30 degrees, Y axis by 30 degrees Z axis by 30 degrees.
-Therefore **R** = Rx . Ry . Rz
+A Repository containing various robotics code
